27e3081b6c8a18fdbd6d96ced451f725ffafaac8
nullbitx8 <nullbitx8@protonmail.com>
2022-07-13 11:48:58 +0100

    add sentry to project and configure it

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 
 // Importing the Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// render App
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// load and init Sentry after rendering App
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN || "",
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: process.env.REACT_APP_SENTRY_SAMPLE_RATE || 1.0,
+  environment: process.env.REACT_APP_SENTRY_ENVIRONMENT || "dev"
+});
+
