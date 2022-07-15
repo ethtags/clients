91edf0fc5ef0d3f6fa37088a4f1e088d6b361dc7
nullbitx8 <nullbitx8@protonmail.com>
2022-07-15 04:15:21 +0100

    make navbar a function-based component

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -8,31 +8,29 @@ import { faTelegram } from '@fortawesome/free-brands-svg-icons';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 
-class ETNavbar extends React.Component {
-  render() {
-    return (
-      <Navbar bg="light">
-        {/* Home & Tagline */}
-        <Container className="justify-content-start">
-          <Navbar.Brand as={Link} to="/">ETHTags</Navbar.Brand>
-          <Navbar.Text>Crowdsourced Nametags</Navbar.Text>
-        </Container>
+function ETNavbar() {
+  return (
+    <Navbar bg="light">
+      {/* Home & Tagline */}
+      <Container className="justify-content-start">
+        <Navbar.Brand as={Link} to="/">ETHTags</Navbar.Brand>
+        <Navbar.Text>Crowdsourced Nametags</Navbar.Text>
+      </Container>
 
-        {/* Socials */}
-        <Container className="justify-content-end">
-          <Navbar.Brand href="#">
-            <FontAwesomeIcon icon={faDiscord} color="#5865f2" />
-          </Navbar.Brand>
-          <Navbar.Brand href="#">
-            <FontAwesomeIcon icon={faTelegram} color="#24a1de" />
-          </Navbar.Brand>
-          <Navbar.Brand href="#">
-            <FontAwesomeIcon icon={faTwitter} color="#1d9bf0" />
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
-    )
-  }
+      {/* Socials */}
+      <Container className="justify-content-end">
+        <Navbar.Brand href="#">
+          <FontAwesomeIcon icon={faDiscord} color="#5865f2" />
+        </Navbar.Brand>
+        <Navbar.Brand href="#">
+          <FontAwesomeIcon icon={faTelegram} color="#24a1de" />
+        </Navbar.Brand>
+        <Navbar.Brand href="#">
+          <FontAwesomeIcon icon={faTwitter} color="#1d9bf0" />
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
+  )
 }
 
 
