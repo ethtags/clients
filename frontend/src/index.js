15e7cd53da604478d502481f134afd46147da425
nullbitx8 <nullbitx8@protonmail.com>
2022-07-28 11:35:58 +0100

    only use error reporting of sentry, reduce bundle size

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,13 +8,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 // importing third party JS asynchronously
 addScript({
-  src: "https://browser.sentry-cdn.com/7.6.0/bundle.tracing.min.js",
+  src: "https://browser.sentry-cdn.com/7.8.0/bundle.min.js",
   id: "sentry-cdn",
   onLoad: () => {
     window.Sentry.init({
       dsn: process.env.REACT_APP_SENTRY_DSN || "",
-      integrations: [new window.Sentry.Integrations.BrowserTracing()],
-      tracesSampleRate: process.env.REACT_APP_SENTRY_SAMPLE_RATE || 1.0,
       environment: process.env.REACT_APP_SENTRY_ENVIRONMENT || "dev"
     });
   }
